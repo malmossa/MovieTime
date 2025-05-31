b5a158a84fbee5f3f5b291fc785caf373685d822
malmossa <malmossa@gmail.com>
2025-05-31 14:56:43 -0700

    completed

---
+++ Models/MovieListResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieTime.Models
+{
+    public class MovieListResponse
+    {
+        public int Page { get; set; }
+        public List<Movie> Results { get; set; } = new List<Movie>();
+        public int TotalPages { get; set; }
+        public int TotalResults { get; set; }
+    }
+}
