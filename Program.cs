57cf2206795f7d426e24db75fa61f629a448c971
malmossa <malmossa@gmail.com>
2025-06-03 08:15:18 -0700

    completed

--- Program.cs
+++ Program.cs
@@ -11,4 +11,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddScoped<TMDBService>();
 
+builder.Services.AddScoped<FavoritesService>();
+
 await builder.Build().RunAsync();
